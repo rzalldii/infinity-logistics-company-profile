4659bb54232bb92d9edb4b4b12d7804cee2091e8
rzalldii <bagaskara409@gmail.com>
2025-10-06 11:17:04 +0700

    v2.9

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,19 @@ Route::post('/send-email', [ContactController::class, 'send'])
     ->middleware(['throttle:1,5', ProtectAgainstSpam::class])
     ->name('send.email');
 
-Route::get('/about', function () { return view('/section/about'); });
-Route::get('/services', function () { return view('/section/services'); });
-Route::get('/news', function () { return view('/section/news'); });
-Route::get('/contact', function () { return view('/section/contact'); });
+Route::get('/about', function () { return view('section.about'); });
+Route::get('/services', function () { return view('section.services'); });
+Route::get('/news', function () { return view('section.news'); });
+Route::get('/contact', function () { return view('section.contact'); });
 
-Route::get('/nvocc', function () { return view('/services/nvocc'); });
-Route::get('/nvocc-schedule', function () { return view('/services/nvocc-schedule'); });
-Route::get('/freight-forwarding', function () { return view('/services/freight-forwarding'); });
-Route::get('/domestic-forwarding', function () { return view('/services/domestic-forwarding'); });
-Route::get('/flexitank-flexibag', function () { return view('/services/flexitank-flexibag'); });
-Route::get('/flexitank-flexibag-details', function () { return view('/services/flexitank-flexibag-details'); });
-Route::get('/project-logistics', function () { return view('/services/project-logistics'); });
-Route::get('/customs-clearance', function () { return view('/services/customs-clearance'); });
+Route::get('/nvocc', function () { return view('services.nvocc'); });
+Route::get('/nvocc-schedule', function () { return view('services.nvocc-schedule'); });
+Route::get('/freight-forwarding', function () { return view('services.freight-forwarding'); });
+Route::get('/domestic-forwarding', function () { return view('services.domestic-forwarding'); });
+Route::get('/flexitank-flexibag', function () { return view('services.flexitank-flexibag'); });
+Route::get('/flexitank-flexibag-details', function () { return view('services.flexitank-flexibag-details'); });
+Route::get('/project-logistics', function () { return view('services.project-logistics'); });
+Route::get('/customs-clearance', function () { return view('services.customs-clearance'); });
 
-Route::get('/terms-and-conditions', function () { return view('/support/terms-and-conditions'); });
-Route::get('/privacy-policy', function () { return view('/support/privacy-policy'); });
+Route::get('/terms-and-conditions', function () { return view('support.terms-and-conditions'); });
+Route::get('/privacy-policy', function () { return view('support.privacy-policy'); });
--- webpack.mix.cjs
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-   .css('resources/css/app.css', 'public/css')
-   .minify('public/js/app.js')
-   .minify('public/css/app.css');
-
-mix.version();
